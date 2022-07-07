66930bd20f20bb058a1226110768d35c9d2d92f9
Kevin Kelley <kkelley622@gmail.com>
2022-07-07 14:58:48 -0500

    commit tree 63bddcbd63dce737480257eba3f2af387d41075f

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([]);
+  const [beltPosition, setBeltPosition] = useState(0);
+  const [money, setMoney] = useState(100);
+  const displayCount = 4;
+
+  useEffect(() => {
+    fetch(API)
+    .then((res) => res.json())
+    .then((data) => setSushis(data))
+  }, [])
+
+  function advanceBeltPosition() {
+    setBeltPosition((beltPosition + displayCount) % sushis.length)
+  }
+
+  function setEaten(piece) {
+    const remainingMoney = money - piece.price
+
+    if(remainingMoney >= 0) {
+    setMoney(remainingMoney)
+    setSushis(sushis.map(
+      sushi => sushi.id === piece.id ? 
+      {...sushi, eaten:true} : sushi))
+    }
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer 
+      sushis={sushis.slice(beltPosition, beltPosition + displayCount)}
+      handleMoreClick={advanceBeltPosition}
+      handleEatSushi={setEaten}
+      />
+      <Table plates={sushis.filter(sushi => sushi.eaten)} money={money}/>
     </div>
   );
 }
